0b5cd833c2a99f48de0d29262863fb5112cc7b46
Mark Smith <smith.h.mark@gmail.com>
2021-11-01 18:35:20 -0400

    more scafolding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use std::env;
 use std::io::{self, BufRead, Write};
+use std::fs;
+
+fn run(souce: &str) {
+}
+
+fn run_file(path: &str) {
+    let source = fs::read_to_string(path).unwrap();
+    run(&source);
+}
 
 fn run_prompt() {
     let stdin = io::stdin();
@@ -13,6 +22,7 @@ fn run_prompt() {
             break;
         } 
         let line = buffer.trim();
+        run(line);
         println!("->{}", line);
         io::stdout().flush().unwrap();
     }
@@ -22,7 +32,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Hello, Welcome to Lox in Rust!");
     if args.len() == 1 {
-        println!("Entering interactive mode:");
+        println!("Entering interactive mode(CTRL+D to exit):");
         run_prompt();
         println!("...Exiting");
 
