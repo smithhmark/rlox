b0032343d3fbd39a2dbaa034132cc73d990e54dc
Mark Smith <smith.h.mark@gmail.com>
2021-11-02 23:57:31 -0400

    gathering tokens mod

--- src/main.rs
+++ src/main.rs
@@ -5,73 +5,78 @@ use std::io::{self, BufRead, Write};
 use std::iter;
 use std::str::Chars;
 
-#[derive(Debug)]
-enum TokenType {
-    // Single-character tokens.
-    LeftParen,
-    RightParen,
-    LeftBrace,
-    RightBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Semicolon,
-    Slash,
-    Star,
+mod tokens {
+    use std::fmt;
+    #[derive(Debug)]
+    pub enum TokenType {
+        // Single-character tokens.
+        LeftParen,
+        RightParen,
+        LeftBrace,
+        RightBrace,
+        Comma,
+        Dot,
+        Minus,
+        Plus,
+        Semicolon,
+        Slash,
+        Star,
 
-    // One or two character tokens.
-    Bang,
-    BangEqual,
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
+        // One or two character tokens.
+        Bang,
+        BangEqual,
+        Equal,
+        EqualEqual,
+        Greater,
+        GreaterEqual,
+        Less,
+        LessEqual,
 
-    // Literals.
-    Identifier,
-    String,
-    Number,
+        // Literals.
+        Identifier,
+        String,
+        Number,
 
-    // Keywords.
-    And,
-    Class,
-    Else,
-    False,
-    Fun,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    Super,
-    This,
-    True,
-    Var,
-    While,
+        // Keywords.
+        And,
+        Class,
+        Else,
+        False,
+        Fun,
+        For,
+        If,
+        Nil,
+        Or,
+        Print,
+        Return,
+        Super,
+        This,
+        True,
+        Var,
+        While,
 
-    EOF,
-}
+        EOF,
+    }
 
-#[derive(Debug)]
-struct Token {
-    kind: TokenType,
-    lexeme: String,
-    line: usize,
-    value: Option<usize>,
-}
+    #[derive(Debug)]
+    pub struct Token {
+        pub kind: TokenType,
+        pub lexeme: String,
+        pub line: usize,
+        pub value: Option<usize>,
+    }
 
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.value {
-            None => write!(f, "{:?} {} {}", self.kind, self.lexeme, "None"),
-            Some(_) => write!(f, "{:?} {} {}", self.kind, self.lexeme, "not yet"),
+    impl fmt::Display for Token {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self.value {
+                None => write!(f, "{:?} {} {}", self.kind, self.lexeme, "None"),
+                Some(_) => write!(f, "{:?} {} {}", self.kind, self.lexeme, "not yet"),
+            }
         }
     }
 }
+
+use tokens::*;
 
 #[derive(Debug)]
 struct ScannerError {
